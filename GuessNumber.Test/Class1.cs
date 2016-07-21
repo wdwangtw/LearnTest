43cddbe34fb20c872b04fb6f475c9027ccc92839
wonder king <wonder king>
2016-07-21 22:55:09 +0800

    Create a new sotution, witch contain a console project and a test project

---
+++ GuessNumber.Test/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GuessNumber.Test
+{
+    public class Class1
+    {
+        [Fact]
+        void test_xunit()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        void test_reference_GuessNumber_project()
+        {
+            GuessNumber guessNumber = new GuessNumber();
+            Assert.True(guessNumber.TestForXunit());
+        }
+    }
+}
---
+++ GuessNumber/GuessNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    public class GuessNumber
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool TestForXunit()
+        {
+            return true;
+        }
+    }
+}
