9ae525930c46d00b235d2d8f16161d95cc9d1e11
wonder king <wonder king>
2016-07-21 23:40:51 +0800

    Get 2A*B when the given number and guessed number have 2 same number for each position.

---
+++ GuessNumber.Test/compare_given_number_and_guessed_number.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using Xunit;
+
+namespace GuessNumber.Test
+{
+    public class compare_given_number_and_guessed_number
+    {
+        [Fact]
+        void should_return_0A4B_when_given_and_guessed_are_total_same()
+        {
+            GuessNumber guessNumber = new GuessNumber();
+            const string givenAndGuessed= "1234";
+            string result = guessNumber.Compare(givenAndGuessed, givenAndGuessed);
+            Assert.Equal("0A4B", result);
+        }
+
+        [Theory]
+        [InlineData("1234", "1256")]
+        [InlineData("1234", "1324")]
+        [InlineData("0534", "7834")]
+        void should_return_2AxB_when_have_2_same_number_for_each_position(string given, string guessed)
+        {
+            GuessNumber guessNumber = new GuessNumber();
+            string result = guessNumber.Compare(given, guessed);
+            Assert.True(Regex.IsMatch(result, @"2A\dB"));
+        }
+    }
+}
--- GuessNumber/GuessNumber.cs
+++ GuessNumber/GuessNumber.cs
@@ -14,6 +14,20 @@ namespace GuessNumber
 
         public string Compare(string given, string guessed)
         {
+            int countOfSameNumberAtSamePosition = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (given[i] == guessed[i])
+                {
+                    countOfSameNumberAtSamePosition ++;
+                }
+            }
+
+            if (countOfSameNumberAtSamePosition == 2)
+            {
+                return "2A0B";
+            }
+
             return "0A4B";
         }
     }
